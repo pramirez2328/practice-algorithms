56fd4f9cd3d05419604792177f22cbb561e08a31
Pedro Ramirez <prlara2000@gmail.com>
2024-04-22 23:27:13 -0500

    buble sort

---
+++ bubleSort.js
@@ -0,0 +1,17 @@
+//buble sort
+
+function bubleSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubleSort([3, 2, 1, 5, 4])); // [1, 2, 3, 4, 5]
